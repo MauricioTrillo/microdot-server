5c1d7dd33c7191fe96e5797f7c22d886e4728ace
MauricioTrillo <102543222+MauricioTrillo@users.noreply.github.com>
2025-11-20 23:29:04 -0300

    Update base.js

--- actividades/ej_03/styles/scripts/base.js
+++ actividades/ej_03/styles/scripts/base.js
@@ -4,3 +4,35 @@ document.addEventListener('DOMContentLoaded', () => {
     const opcionesFecha = { year: 'numeric', month: 'long', day: 'numeric' };
     fechaElemento.textContent = fechaActual.toLocaleDateString('es-ES', opcionesFecha);
 });
+
+async function readTemperature() {
+    const resp = await fetchData('/sensors/ds18b20/read');
+    const tempOut = document.getElementById("mostrar-temperatura");
+
+    if (resp) {
+        tempOut.innerText = resp.temperatura;
+    } else {
+        tempOut.innerText = "Error";
+    }
+}
+
+async function sendSetpoint() {
+    const val = Number(document.getElementById("setpoint-slider").value);
+    const resp = await fetchData(`/setpoint/set/${val}`);
+    const buz = document.getElementById("estado-buzzer");
+
+    if (resp) {
+        buz.innerText = resp.buzzer;
+    } else {
+        buz.innerText = "Error";
+    }
+}
+
+function updateSetpointValue(v) {
+    document.getElementById("setpoint-value").innerText = v;
+    sendSetpoint();
+}
+
+setInterval(readTemperature, 1000);
+
+readTemperature();
